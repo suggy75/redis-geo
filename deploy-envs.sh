4e4e63b139a6a95f2607bbb1a262c6eaffd6851d
sug <suggy75@gmail.com>
2018-08-01 20:46:16 +0100

    Updated image name on deploy-envs.sh

--- deploy-envs.sh
+++ deploy-envs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # set environment variables used in deploy.sh and AWS task-definition.json:
-export IMAGE_NAME=netcoreapps-redisgeo
+export IMAGE_NAME=ecsdemo-redisgeo
 export IMAGE_VERSION=latest
 
 export AWS_DEFAULT_REGION=eu-west-1
